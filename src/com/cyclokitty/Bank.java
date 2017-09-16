97b3707ef0d727441faa2898040df5d445859a0e
Laura Vingada <laura@vingada.com>
2017-09-16 16:20:34 -0400

    Adding bank class and get it working.

--- src/com/cyclokitty/Bank.java
+++ src/com/cyclokitty/Bank.java
@@ -1,4 +1,13 @@
 package com.cyclokitty;
 
+import java.util.ArrayList;
+
 public class Bank {
+    private ArrayList<Branch> branches;
+    private ArrayList<Customer> customers;
+
+    public Bank(ArrayList<Branch> branches, ArrayList<Customer> customers) {
+        this.branches = new ArrayList<Branch>();
+        this.customers = new ArrayList<Customer>();
+    }
 }
